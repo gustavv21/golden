e06fdcd7ac48c4f17e18ae64367dc863991aa29a
Łukasz <biernackilukasz97@gmail.com>
2018-11-29 20:28:23 +0000

    update home and game

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
-var express = require("express");
-var app = express();
+var express         = require("express"),
+    app             = express(),
+    bodyparser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose");
 
+mongoose.connect("mongodb://localhost/golden", { useNewUrlParser: true });
+app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride("_method"));
+
+var gameSchema = new mongoose.Schema({
+    player: String,
+    player2: String,
+    price: Number
+});
+
+var Game = mongoose.model("Game", gameSchema);
 
 app.get("/", function(req,res){
     res.render("home");
 });
 
 app.get("/gra", function(req,res){
-    res.render("gra");
+    Game.find({}, function(err, game){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("game/gra", {game:game});
+        }
+    });
 });
-app.get("/ee", function(req, res) {
-    res.render("ee")
-})
+
+app.post("/gra", function(req, res) {
+    var player  = req.body.player;
+    var player2 = req.body.player2;
+    var price   = req.body.price;
+    var full    = {player:player, player2:player2, price:price};
+    Game.create(full, function(err,full){
+        if(err){
+            res.render("game/gra");
+        } else {
+            res.redirect("/gra");
+        }
+    });
+});
+
+app.get("/gra/:id", function(req, res) {
+    Game.findById(req.params.id, function(err,foundGame){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("game/show", {game:foundGame});
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("serwer is on");
